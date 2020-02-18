c7670bb8b521f766e97525be435fb5afea4ec94c
BigFun123 <57192792+BigFun123@users.noreply.github.com>
2020-02-18 21:58:03 +0200

    Ps 123] new feature (#1)
    * [ed1] added editor
    
    * [ed1] added editor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 //import './bunny.js'
 //import './clouds.js'
 import './clouds5.js'
-import '../staging/typetest.js'
+import './shaderplay.js';
 
 var INDEX = 0;
 export default INDEX;
--- src/shaderplay.js
+++ src/shaderplay.js
@@ -1,13 +1,19 @@
 console.log("shaderplay 1");
+import {SetFrag,frag} from './clouds5.js';
 
-setInterval(()=>{
-    getText();
-}, 1000);
-
+$(document).ready(()=>
+{
+    $('textarea#tarea').val(frag);
+})
 function getText() {
     let ta = document.getElementById("tarea");
-    console.log("G" + ta.value);
+    return ta.innerHTML;
 }
+
+$('#tarea').bind('input propertychange', function() {
+
+    SetFrag( $('textarea#tarea').val());
+});
 
 let SHADERPLAY = null;
 export default SHADERPLAY;
